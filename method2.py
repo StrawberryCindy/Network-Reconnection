423228d0e73c100051358608721d0a5506f7070b
StrawberryCindy <1542508429@qq.com>
2021-07-09 14:29:28 +0800

    终于做出了2018pathcost的一部分

--- method2.py
+++ method2.py
@@ -6,7 +6,8 @@ from operator import itemgetter
 from shapely.geometry import Polygon
 
 
-def make_mesh(box, w, h):  # 按照w,h对box进行网格划分
+# 按照w,h对box进行网格划分 #################
+def make_mesh(box, w, h):
     [xmin, ymin, xmax, ymax] = box
     list_x=np.arange(xmin, xmax, w)
     list_y=np.arange(ymin, ymax, h)
@@ -29,14 +30,27 @@ def make_mesh(box, w, h):  # 按照w,h对box进行网格划分
     return gpd.GeoSeries(polygon_list)
 
 
+# 将数组元素解析成字典 ###############
+def to_obj(arr):
+    obj = [{} for i in  range(len(arr))]
+    for index, item in enumerate(arr):
+        obj[index]['x'] = item[0]
+        obj[index]['y'] = item[1]
+        obj[index]['block'] = 0   # 所属区域块编号
+        obj[index]['type'] = 'N'  # N为普通点，B为边界点
+        obj[index]['movable'] = False   # 是否可移动
+    return obj
+
+
+# 迭代生成子点 #################
 def init(radius, center):
-    x_center = center[0]
-    y_center = center[1]
+    x_center = center['x']
+    y_center = center['y']
     while True:
         x = x_center + (random.random() - 0.5) * radius * 2
         y = y_center + (random.random() - 0.5) * radius * 2
         if (x - x_center) ** 2 + (y - y_center) ** 2 <= radius ** 2 and 0 < x < xm and 0 < y < ym:
-            return [x, y]
+            return x, y
 
 
 # 判断边界点 ###################
@@ -45,22 +59,22 @@ def get_border(nodes):
     for index, node in enumerate(nodes):
         angle_r_all = []
         right_max = 0
-        S2 = S[:]
+        S2 = nodes[:]
         del S2[index]  # 中间变量，存放除本节点外的其他点
         for other_node in S2:
-            if np.abs(other_node[0] - node[0]) < 2 * R and np.abs(other_node[1] - node[1]) < 2 * R:
-                d = np.sqrt((node[0] - other_node[0]) ** 2 + (node[1] - other_node[1]) ** 2)
+            if np.abs(other_node['x'] - node['x']) < 2 * R and np.abs(other_node['y'] - node['y']) < 2 * R:
+                d = np.sqrt((node['x'] - other_node['x']) ** 2 + (node['y'] - other_node['y']) ** 2)
                 if d < 2 * R:
                     angle_d = np.arccos(((d / 2) / R))
-                    if node[0] > other_node[0]:
-                        center_angle = np.arctan((other_node[1] - node[1]) / (other_node[0] - node[0])) + np.pi
-                    elif node[0] == other_node[0]:
-                        if node[1] > other_node[1]:
+                    if node['x'] > other_node['x']:
+                        center_angle = np.arctan((other_node['y'] - node['y']) / (other_node['x'] - node['x'])) + np.pi
+                    elif node['x'] == other_node['x']:
+                        if node['y'] > other_node['y']:
                             center_angle = -np.pi / 2
                         else:
                             center_angle = np.pi / 2
                     else:
-                        center_angle = np.arctan((other_node[1] - node[1]) / (other_node[0] - node[0]))
+                        center_angle = np.arctan((other_node['y'] - node['y']) / (other_node['x'] - node['x']))
                     # 左右边界
                     left = center_angle - angle_d
                     right = center_angle + angle_d
@@ -70,16 +84,86 @@ def get_border(nodes):
         angle_r_all = sorted(angle_r_all, key=itemgetter(0))  # 按left升序排序
         if angle_r_all[0][0] >= -np.pi / 2:
             border_nodes.append(node)
+            nodes[index]['type'] = 'B'
             continue
         for i in range(0, len(angle_r_all)):
             if angle_r_all[i][0] > right_max:  # 当左端值不与右端当前最大值发生重合时，判为边界点
                 border_nodes.append(node)
+                nodes[index]['type'] = 'B'
                 break
             if angle_r_all[i][1] > right_max:  # 如果此右端点大于右端当前最大值，进行迭代
                 right_max = angle_r_all[i][1]
         if right_max < np.pi * 3 / 2:
             border_nodes.append(node)
-    return border_nodes
+            nodes[index]['type'] = 'B'
+    return border_nodes, nodes
+
+
+def create_nodes(c):  # c母点集，返回生成的所有点S， 以及block：按区域划分的二维点集
+    s = []
+    block = [[0]*n for index in range(len(c))]
+    for j, c_node in enumerate(c):
+        i = 0
+        while i < n:
+            i = i+1
+            new_node = {}
+            new_node['x'],new_node['y'] = init(R, c_node)
+            new_node['block'] = j
+            new_node['type'] = 'N'  # N为普通点，B为边界点
+            new_node['movable'] = False   # 是否可移动
+            block[j][i - 1] = c_node
+            c_node = new_node
+            s.append(new_node)
+    return s, block
+
+
+def draw_nodes(nodes):  # 画所有点
+    for i in nodes:
+        if i['movable']:
+            plt.plot(i['x'], i['y'], 'mv')
+        elif i['type'] == 'B':
+            plt.plot(i['x'], i['y'], 'c.')
+        else:
+            plt.plot(i['x'], i['y'], 'k.')
+
+
+def get_move_2016(s, blocks, mob_n):  # blocks 按区域划分的二维点集，mob_n区域中的可移动节点数
+    for i in range(0, len(blocks)):
+        j = 0
+        while j < mob_n:
+            j = j+1
+            index = int(random.random()*n)
+            s[index + i*n]['movable'] = True
+    return s
+
+
+def sort_border(border, cl):
+    border2 = [[] for index in range(cl)]  # cl母点集长度
+    for node in border:
+        bi = node['block']
+        border2[bi].append(node)
+    return border2
+
+
+def get_d_2018(border):   # blocks 按区域划分的二维点集
+    d_all = 0
+    for nodes in border:
+        d_min = 1000000
+        for node in nodes:
+            d = (node['x']-sink['x'])**2 + (node['y']-sink['y'])**2
+            node['d'] = d
+            if d < d_min:
+                d_min = d
+            else:
+                continue
+        d_all = d_all + np.sqrt(d_min) - R   # 圆桌协议过程中移动的全路径
+    return border, d_all
+
+
+def get_min_path_2018(blocks):
+    for nodes in blocks:
+        for node in nodes:
+            return 0
 
 
 # PARAMETERS ##############################
@@ -87,7 +171,7 @@ xm = 1000    # 横坐标长度
 ym = 1000   # 纵坐标长度
 sink = {'x': 0, 'y': 0}   # 基站定义
 sink['x'] = xm/2  # 基站横坐标
-sink['y'] = ym-10  # 基站纵坐标
+sink['y'] = ym-50  # 基站纵坐标
 n = 16   # 每个区域的节点个数
 R = 30  # 节点通信半径
 [w, h] = [50, 50]  # 网格长宽
@@ -97,29 +181,26 @@ R = 30  # 节点通信半径
 C_20 = [(50, 100), (100, 400), (50, 700), (30, 950), (300, 30), (340, 350), (260, 680), (280, 890),
         (500, 200), (500, 550), (590, 720), (570, 900), (730, 100), (600, 400), (780, 830),
         (950, 30), (840, 300), (870, 500), (980, 700), (950, 950)]
+C_20 = to_obj(C_20)
 
 C_15 = [(50, 100), (100, 400), (50, 700), (50, 900), (340, 340), (400, 660),
         (500, 950), (500, 200), (590, 720), (730, 100), (650, 480), (780, 830), (890, 30), (870, 400), (900, 950)]
+C_15 = to_obj(C_15)
 
-S = C_15[:]  # 节点集
-for c_node in C_15:
-    i = 0
-    while i < n:
-        i = i+1
-        new_node = init(R, c_node)
-        c_node = new_node
-        S.append(new_node)
+# 当区域数为15时 ###########################
+S_15, block_15 = create_nodes(C_15)
 
-B = get_border(S)  # 边界点
+B_15, S_15 = get_border(S_15)  # 边界点
+B_15 = sort_border(B_15, len(C_15))  # 按区域划分的二维边界点集合
+
+S_15_2016 = get_move_2016(S_15, block_15, 3)
+block_15_2018, d_cost_2018_1 = get_d_2018(B_15)  # 终于产生了第一个能用的实验数据呜呜呜o(╥﹏╥)o
+print(d_cost_2018_1)
 
 # 作图  ####################################
 gdf = make_mesh([0, 0, xm, ym], w, h)
 gdf.boundary.plot()
-for i in S:
-    plt.plot(i[0], i[1], 'k.')
-
-for i in B:
-    plt.plot(i[0], i[1], 'b.')
+draw_nodes(S_15)
 
 plt.plot(sink['x'], sink['y'], 'rp')  # 绘制sink点
 plt.annotate('sink', xy=(sink['x'], sink['y']), xytext=(-20, 10),
